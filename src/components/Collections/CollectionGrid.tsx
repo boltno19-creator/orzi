46c7cf30455a7fd5c54cef40c97f436b47f1671e
boltno19-creator <boltno19@gmail.com>
2026-02-06 21:00:31 +0200

    Updated CollectionGrid.tsx

--- src/components/Collections/CollectionGrid.tsx
+++ src/components/Collections/CollectionGrid.tsx
@@ -16,7 +16,7 @@ export default function CollectionGrid({
             ORZI خط
           </h2>
           <p className="text-lg text-gray-700 max-w-2xl mx-auto">
-          قطع فنية مُختارة بدقة.. وقادمة قريباً
+          .قطع فنية مُختارة بدقة.. قادمة قريباً
           </p>
         </div>
 
